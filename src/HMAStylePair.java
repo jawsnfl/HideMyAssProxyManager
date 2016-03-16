413ed953cc0a2ab088d36da61510798b77774644
datzilla <datzilla@gmail.com>
2016-03-16 15:26:55 +1100

    Start of the project

---
+++ src/HMAStylePair.java
@@ -0,0 +1,31 @@
+
+public class HMAStylePair {
+	
+	private String mKey;
+	private boolean mDisplayFlag = false;
+	
+	HMAStylePair (String pKey, String pFlag) {
+		this.mKey = pKey;
+		if (pFlag.equalsIgnoreCase("none")) {
+			this.mDisplayFlag = false;
+		} else {
+			this.mDisplayFlag = true;
+		}
+	}
+
+	public String getKey() {
+		return mKey;
+	}
+
+	public boolean isDisplayFlag() {
+		return mDisplayFlag;
+	}
+
+	public void setKey(String pKey) {
+		mKey = pKey;
+	}
+
+	public void setDisplayFlag(boolean pDisplayFlag) {
+		mDisplayFlag = pDisplayFlag;
+	}	
+}
